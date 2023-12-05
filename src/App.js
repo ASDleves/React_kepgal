6082047dd7b225e5d70362bd7942fb1a38ff9cc1
ASDleves <ricsi20020302@gmail.com>
2023-12-05 11:40:49 +0100

    kepgaleria

---
+++ src/App.js
@@ -0,0 +1,35 @@
+
+import './App.css';
+import Kep from './komponensek/Kep';
+import { kepLista } from './KepLista';
+import {useState} from 'react';
+function App() {
+
+const [aktKep, setAktKep] = useState(0)
+
+  function kattintasKezeles(index){
+    console.log(index)
+    setAktKep(index)
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Képgaléria</h1>
+      </header>
+      <section>
+    <Kep src={kepLista[aktKep]} index={aktKep} kattintasKezeles={kattintasKezeles}/>
+      </section>
+      <article>
+      {kepLista.map((elem, index)=>{
+      return(<Kep src={elem}key={index} index={index} kattintasKezeles={kattintasKezeles}/>)
+      })
+      }
+
+      </article>
+      <footer>Bella Richárd</footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/KepLista.js
@@ -0,0 +1,6 @@
+export const kepLista = [
+    "./kepek/kep1.jpg",
+    "./kepek/kep2.jpg",
+    "./kepek/kep3.jpg",
+
+]
